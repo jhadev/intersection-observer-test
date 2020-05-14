ddd2db5a5c469f276b73aa40076f904e1d2a0957
jhadev <joshappeldev@gmail.com>
2020-05-14 09:36:08 -0400

    test

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+// check for browser support
+
+if ('IntersectionObserver' in window) {
+  // find video tag
+  const video = document.querySelector('video');
+  // set a flag for pausing
+  let isPaused = false;
+
+  // create new instance of IntersectionObserver
+  let observer = new IntersectionObserver(
+    (entries, observer) => {
+      // entries are an array
+      entries.forEach((entry) => {
+        /*
+        The IntersectionObserverEntry interface's read-only intersectionRatio property tells you how much of the target element is currently visible within the root's intersection ratio, as a value between 0.0 and 1.0.
+      */
+        // if video is not is full view and isn't paused
+        if (entry.intersectionRatio !== 1 && !video.paused) {
+          // pause the video
+          video.pause();
+          // set paused flag to true
+          isPaused = true;
+        } else if (isPaused) {
+          // if video is paused already
+          // play video
+          video.play();
+          // set flag back to false
+          isPaused = false;
+        }
+      });
+      console.log(entries);
+      console.log(observer);
+      // run when target element has intersectionRatio of 1
+    },
+    { threshold: 1 }
+  );
+  observer.observe(video);
+}
