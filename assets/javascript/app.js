86eea56115e7d329646b070bd4c88ccd5e8d7b0d
jhadev <joshappeldev@gmail.com>
2020-05-23 16:13:26 -0400

    extract callback

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,43 +2,45 @@
 const videos = document.querySelectorAll('video');
 console.log(videos);
 
+const flag = (video, isPaused) => {
+  // entries are an array
+
+  return (entries) => {
+    entries.forEach((entry) => {
+      /*
+    The IntersectionObserverEntry interface's read-only intersectionRatio property 
+    tells you how much of the target element is currently visible within the root's intersection ratio,
+     as a value between 0.0 and 1.0.
+  */
+
+      // if video is not is full view and isn't paused
+      if (entry.intersectionRatio !== 1 && !video.paused) {
+        // pause the video
+        video.pause();
+        // set paused flag to true
+        isPaused = true;
+      } else if (isPaused) {
+        // if video is paused already
+        // play video
+        video.play();
+        // set flag back to false
+        isPaused = false;
+      }
+    });
+    console.log(entries);
+  };
+};
+
 // function for autoPlay/pause will return observer
 const auto = (video) => {
   // set a flag for pausing
   let isPaused = false;
 
+  const videoFlag = flag(video, isPaused);
+
   // create new instance of IntersectionObserver
-  let observer = new IntersectionObserver(
-    (entries, observer) => {
-      // entries are an array
-
-      entries.forEach((entry, index) => {
-        /*
-        The IntersectionObserverEntry interface's read-only intersectionRatio property 
-        tells you how much of the target element is currently visible within the root's intersection ratio,
-         as a value between 0.0 and 1.0.
-      */
-
-        // if video is not is full view and isn't paused
-        if (entry.intersectionRatio !== 1 && !video.paused) {
-          // pause the video
-          video.pause();
-          // set paused flag to true
-          isPaused = true;
-        } else if (isPaused) {
-          // if video is paused already
-          // play video
-          video.play();
-          // set flag back to false
-          isPaused = false;
-        }
-      });
-      console.log(entries);
-      console.log(observer);
-      // run when target element has intersectionRatio of 1
-    },
-    { threshold: 1 }
-  );
+  // run when target element has intersectionRatio of 1
+  let observer = new IntersectionObserver(videoFlag, { threshold: 1 });
 
   return observer;
 };
